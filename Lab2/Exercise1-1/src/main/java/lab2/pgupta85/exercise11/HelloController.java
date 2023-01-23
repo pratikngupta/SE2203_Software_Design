a26ac6345be82db22c6da4bbf9396796bd13bf60
Pratik Narendra Gupta <pratikngupta@outlook.com>
2023-01-23 08:19:49 -0500

    Added base file for Exercise1-1

---
+++ Lab2/Exercise1-1/src/main/java/lab2/pgupta85/exercise11/HelloController.java
@@ -0,0 +1,14 @@
+package lab2.pgupta85.exercise11;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ Lab2/Exercise1-1/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module lab2.pgupta85.exercise11 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens lab2.pgupta85.exercise11 to javafx.fxml;
+    exports lab2.pgupta85.exercise11;
+}
