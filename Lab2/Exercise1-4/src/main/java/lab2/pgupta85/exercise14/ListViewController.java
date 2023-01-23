a94b2a664f5ff24343812682a9fa793af839d6c4
Pratik Narendra Gupta <pratikngupta@outlook.com>
2023-01-23 11:59:39 -0500

    Got multiple selection to work
    Now we can select multiple items from the list and it will display in the label

--- Lab2/Exercise1-4/src/main/java/lab2/pgupta85/exercise14/ListViewController.java
+++ Lab2/Exercise1-4/src/main/java/lab2/pgupta85/exercise14/ListViewController.java
@@ -1,13 +1,19 @@
 package lab2.pgupta85.exercise14;
 
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+
+import java.util.ArrayList;
 
 public class ListViewController {
 
+    public javafx.scene.control.Button Button;
     @FXML
     private Label selectedItem;
     @FXML
@@ -15,12 +21,23 @@ public class ListViewController {
 
     // This method is called by the FXMLLoader when initialization is complete
     public void initialize() {
-        lvItem.getItems().addAll("Poodle", "Great Dane", "Labrador", "Terrier");
+        ArrayList<String> strArrayList = new ArrayList<>();
+        strArrayList.add("Poodle");
+        strArrayList.add("Great Dane");
+        strArrayList.add("Labrador");
+        strArrayList.add("Terrier");
+        ObservableList<String> strList = FXCollections.observableArrayList(strArrayList);
+        lvItem.getItems().setAll(strList);
+
     }
+
     @FXML
     void displaySelectedItem() {
-        selectedItem.setText(lvItem.getSelectionModel().getSelectedIndex() + "-" +
-                lvItem.getSelectionModel().getSelectedItem());
+        //Multiple selection
+        lvItem.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        ObservableList<String> selectedItems = lvItem.getSelectionModel().getSelectedItems();
+        selectedItem.setText(selectedItems.toString());
+
     }
 
 }
