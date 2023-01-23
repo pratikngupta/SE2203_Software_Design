e0107a094870d26147482a7e38a257b305528823
Pratik Narendra Gupta <pratikngupta@outlook.com>
2023-01-23 12:59:51 -0500

    Created basic GUI

--- Lab2/Exercise3/src/main/java/lab2/pgupta85/exercise3/SliderController.java
+++ Lab2/Exercise3/src/main/java/lab2/pgupta85/exercise3/SliderController.java
@@ -2,13 +2,24 @@ package lab2.pgupta85.exercise3;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.input.InputMethodEvent;
+import javafx.scene.input.MouseEvent;
 
 public class SliderController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Label celsiusBox;
+
+    @FXML
+    private Slider celsiusSlider;
+
+    @FXML
+    private Label fahrenheitBox;
+
+    @FXML
+    void CelsiusToFah(MouseEvent event) {
+
     }
+
 }
