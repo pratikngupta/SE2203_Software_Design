82631a194befc376d97f7527915678fb701c2095
Pratik Narendra Gupta <pratikngupta@outlook.com>
2023-01-23 08:43:20 -0500

    Building base GUI

--- Lab2/Exercise1-1/src/main/java/lab2/pgupta85/exercise11/ListViewApplication.java
+++ Lab2/Exercise1-1/src/main/java/lab2/pgupta85/exercise11/ListViewApplication.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class ListViewApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(ListViewApplication.class.getResource("ListView-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
---
+++ Lab2/Exercise1-1/src/main/java/lab2/pgupta85/exercise11/ListViewController.java
@@ -0,0 +1,13 @@
+package lab2.pgupta85.exercise11;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+public class ListViewController {
+
+    @FXML
+    private ListView<String> lvItem;
+
+     listView.getItems().addAll("Poodle", "Great Dane", "Labrador", "Terrier");
+
+}
