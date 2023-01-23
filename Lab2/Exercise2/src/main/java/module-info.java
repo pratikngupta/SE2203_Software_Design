2a8e91a4846e4199c42f8146796bb69ddf8a7d62
Pratik Narendra Gupta <pratikngupta@outlook.com>
2023-01-23 08:27:56 -0500

    Added base file for Exercise2

---
+++ Lab2/Exercise2/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module lab2.pgupta85.exercise2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens lab2.pgupta85.exercise2 to javafx.fxml;
+    exports lab2.pgupta85.exercise2;
+}
