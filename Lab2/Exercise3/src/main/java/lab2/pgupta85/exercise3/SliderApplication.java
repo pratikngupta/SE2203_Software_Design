6c8df27ac637f95ca1d7c921212769bcefc66ea4
Pratik Narendra Gupta <pratikngupta@outlook.com>
2023-01-23 13:10:33 -0500

    Working GUI

--- Lab2/Exercise3/src/main/java/lab2/pgupta85/exercise3/SliderApplication.java
+++ Lab2/Exercise3/src/main/java/lab2/pgupta85/exercise3/SliderApplication.java
@@ -12,7 +12,7 @@ public class SliderApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(SliderApplication.class.getResource("Slider-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        stage.setTitle("Converter");
         stage.setScene(scene);
         stage.show();
     }
