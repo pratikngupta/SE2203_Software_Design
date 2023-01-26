7090ba6dce4b909db165e74679c1267a0f43e1b8
Pratik Narendra Gupta <pratikngupta@outlook.com>
2023-01-26 14:46:36 -0500

    learning to create .jar file

--- Lab2/Exercise3/src/main/java/lab2/pgupta85/exercise3/SliderApplication.java
+++ Lab2/Exercise3/src/main/java/lab2/pgupta85/exercise3/SliderApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+//import fx:deploy to create a jar file
 
 import java.io.IOException;
 
