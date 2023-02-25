9035f4956147a3e40a5a3414229dbfa63bb7ff35
Pratik Narendra Gupta <pratikngupta@outlook.com>
2023-02-25 16:08:31 -0500

    working on pop up window

--- Assignment 2/iFINANCE/src/main/java/assignment2/pgupta85/ifinance/iFinanceController.java
+++ Assignment 2/iFINANCE/src/main/java/assignment2/pgupta85/ifinance/iFinanceController.java
@@ -1,14 +1,40 @@
 package assignment2.pgupta85.ifinance;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
 
 public class iFinanceController {
+
+
+    public MenuItem AboutUs;
     @FXML
-    private Label welcomeText;
+    private Menu aboutMenu;
+
+
+    public void initialize() {
+    }
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    void openAboutMenu(ActionEvent event) {
+        //when the menu is clicked, show the about menu
+        aboutMenu.show();
+    }
+
+    //when about us is clicked, show the about us page
+    @FXML
+    void openAboutUs() {
+        System.out.println("About Us");
+    }
+
+    public void AboutUsClicked() {
+        //open new pop up window with about us info
+        System.out.println("About Us");
+
+        //pop up new window with about us info
+
+
+        
     }
 }
