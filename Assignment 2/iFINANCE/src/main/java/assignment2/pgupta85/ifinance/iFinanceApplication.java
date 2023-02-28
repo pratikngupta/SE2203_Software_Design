61bfc22bcf136ea06ed2e673821e53bc1340740f
Pratik Narendra Gupta <pratikngupta@outlook.com>
2023-02-28 17:30:06 -0500

    Finished the assignment

--- Assignment 2/iFINANCE/src/main/java/assignment2/pgupta85/ifinance/iFinanceApplication.java
+++ Assignment 2/iFINANCE/src/main/java/assignment2/pgupta85/ifinance/iFinanceApplication.java
@@ -3,6 +3,7 @@ package assignment2.pgupta85.ifinance;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,9 +12,10 @@ public class iFinanceApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(iFinanceApplication.class.getResource("iFINANCE-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 1300, 725);
+        stage.setTitle("iFinance");
         stage.setScene(scene);
+        stage.getIcons().add(new Image(iFinanceApplication.class.getResourceAsStream("WesternLogo.png")));
         stage.show();
     }
 
--- Assignment 2/iFINANCE/src/main/java/assignment2/pgupta85/ifinance/iFinanceController.java
+++ Assignment 2/iFINANCE/src/main/java/assignment2/pgupta85/ifinance/iFinanceController.java
@@ -2,39 +2,89 @@ package assignment2.pgupta85.ifinance;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuItem;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+import static javafx.application.Platform.exit;
+
 
 public class iFinanceController {
 
-
-    public MenuItem AboutUs;
     @FXML
-    private Menu aboutMenu;
-
+    private BorderPane MainStage;
+    @FXML
+    private Stage aboutUsStage = new Stage();
 
     public void initialize() {
+        //set WesternBackground.png as background
+        MainStage.setStyle(
+                "-fx-background-image: url(" +
+                        Objects.requireNonNull(getClass().getResource("WesternBackground.png")) +
+                        "); " +
+                        "-fx-background-size: 100% 100%;"
+        );
     }
 
     @FXML
-    void openAboutMenu(ActionEvent event) {
-        //when the menu is clicked, show the about menu
-        aboutMenu.show();
+    void AboutUsClicked( ) {
+        createAboutUs();
     }
 
-    //when about us is clicked, show the about us page
     @FXML
-    void openAboutUs() {
-        System.out.println("About Us");
+    void CloseOptionClicked() {
+        exit();
     }
 
-    public void AboutUsClicked() {
-        //open new pop up window with about us info
-        System.out.println("About Us");
+    //create a method to align the elements in the anchor pane which will be used in the AboutUsClicked method
+    public void alignElements(Label label, int x, int y) {
+        label.setLayoutX(x);
+        label.setLayoutY(y);
+    }
+    //create a generic method to align the elements in the anchor pane which will be used in the AboutUsClicked method
+    public void alignElements(Button button, int x, int y) {
+        button.setLayoutX(x);
+        button.setLayoutY(y);
+    }
 
-        //pop up new window with about us info
+    public void createAboutUs(){
+        //create a new stage
+        aboutUsStage.setTitle("About Us");
+        aboutUsStage.getIcons().add(new javafx.scene.image.Image(Objects.requireNonNull(getClass().getResourceAsStream("WesternLogo.png"))));
 
+        AnchorPane AboutUsvbox = new AnchorPane();
+        Label companyLabel = new Label("iFinance");
+        Label copyRightLabel = new Label("Copyright @2023 SE2203b");
+        Label name = new Label("Pratik Narendra Gupta");
+        Button close = new Button("Close");
 
-        
+        //set the size of the vbox
+        AboutUsvbox.setPrefSize(500, 500);
+
+        //align the elements in the vbox in center of the vbox
+        alignElements(companyLabel, 228, 203);
+        alignElements(copyRightLabel, 171, 233);
+        alignElements(name, 188, 263);
+
+        alignElements(close, 223, 310);
+
+        //set name color to red
+        name.setStyle("-fx-text-fill: red");
+
+        //set the action of the button
+        close.setOnAction(event -> aboutUsStage.close());
+
+        //set the scene of the stage
+        aboutUsStage.setScene(new Scene(AboutUsvbox));
+        //Add all the elements to the vbox
+        AboutUsvbox.getChildren().addAll(companyLabel, copyRightLabel, name, close);
+
+        //Launch the stage
+        aboutUsStage.show();
     }
 }
