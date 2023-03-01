70a0eef685a61897aa70ba9e9ab99a0e06f48dc2
Pratik Narendra Gupta <pratikngupta@outlook.com>
2023-03-01 05:42:08 -0500

    Trying to implement runable

--- Assignment 2/iFINANCE/src/main/java/assignment2/pgupta85/ifinance/AboutUs.java
+++ Assignment 2/iFINANCE/src/main/java/assignment2/pgupta85/ifinance/AboutUs.java
@@ -5,54 +5,19 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.util.Objects;
 
-import static javafx.application.Platform.exit;
-
-
-public class iFinanceController {
-
-    @FXML
-    private BorderPane MainStage;
-    @FXML
-    private Stage aboutUsStage = new Stage();
-
-    public void initialize() {
-        //set WesternBackground.png as background
-        MainStage.setStyle(
-                "-fx-background-image: url(" +
-                        Objects.requireNonNull(getClass().getResource("WesternBackground.png")) +
-                        "); " +
-                        "-fx-background-size: 100% 100%;"
-        );
-    }
-
-    @FXML
-    void AboutUsClicked( ) {
-        createAboutUs();
-    }
-
-    @FXML
-    void CloseOptionClicked() {
-        exit();
-    }
-
-    //create a method to align the elements in the anchor pane which will be used in the AboutUsClicked method
-    public void alignElements(Label label, int x, int y) {
-        label.setLayoutX(x);
-        label.setLayoutY(y);
-    }
-    //create a generic method to align the elements in the anchor pane which will be used in the AboutUsClicked method
-    public void alignElements(Button button, int x, int y) {
-        button.setLayoutX(x);
-        button.setLayoutY(y);
-    }
+public class AboutUs implements Runnable {
 
     public void createAboutUs(){
-        //Launch the stage
+
+        //create a event thread
+        Thread eventThread = new Thread(this);
+        eventThread.start();
+        Stage aboutUsStage = new Stage();
+
         aboutUsStage.setTitle("About Us");
         aboutUsStage.getIcons().add(new javafx.scene.image.Image(Objects.requireNonNull(getClass().getResourceAsStream("WesternLogo.png"))));
 
@@ -84,5 +49,26 @@ public class iFinanceController {
         AboutUsvbox.getChildren().addAll(companyLabel, copyRightLabel, name, close);
 
         aboutUsStage.show();
+    }
+
+    private void alignElements(Label label, double x, double y) {
+        label.setLayoutX(x);
+        label.setLayoutY(y);
+    }
+
+    private void alignElements(Button button, double x, double y) {
+        button.setLayoutX(x);
+        button.setLayoutY(y);
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    public static void main(String[] args) {
+        AboutUs aboutUs = new AboutUs();
+        aboutUs.createAboutUs();
+
     }
 }
